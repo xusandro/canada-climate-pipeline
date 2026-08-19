4cbe8f86429b972a644748d6d1f99118cf6f3da5
xusandro <xu01sandro@gmail.com>
2026-08-19 14:02:34 +0200

    Build yearly mart from the fact table and cast date parts to int

--- dbt_climate/models/marts/dim_date.sql
+++ dbt_climate/models/marts/dim_date.sql
@@ -5,9 +5,9 @@ with timeline as (
 
 select
     date_day,
-    date_part(year,  date_day) as year,
-    date_part(month, date_day) as month,
-    date_part(day,   date_day) as day,
+    date_part(year,  date_day)::int as year,
+    date_part(month, date_day)::int as month,
+    date_part(day,   date_day)::int as day,
     case when date_part(month, date_day) between 3 and 5 then 'spring'
          when date_part(month, date_day) between 6 and 8 then 'summer'
          when date_part(month, date_day) between 9 and 11 then 'fall'
--- dbt_climate/models/marts/mart_yearly_climate.sql
+++ dbt_climate/models/marts/mart_yearly_climate.sql
@@ -1,5 +1,5 @@
 select 
-date_part(year, observation_date) as observation_year,
+date_part(year, observation_date)::int as observation_year,
 count(distinct station_id) as station_count,
 count(tmax_c) as tmax_days,
 count(station_id) as station_days,
@@ -9,5 +9,5 @@ avg(tavg_c) as avg_tavg_c,
 avg(prcp_mm) as avg_prcp_mm,
 avg(snow_fall_mm) as avg_snow_mm,
 avg(snwd_depth_mm) as avg_snwd_mm
-from {{ ref('stg_observations') }}
+from {{ ref('fct_daily_weather') }}
 group by date_part(year, observation_date)
